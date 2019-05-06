fe1187ddcb67c560f59c3af4356da55d97702976
525hanlee <525hanlee@gmail.com>
2019-05-07 02:58:13 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -24,19 +24,19 @@ function createCard(event){
     url = url.replace("http", "https");
     if (data[i].Zone == location) {
       str +='<div class="card">';
-      str +='<div class="ad-location">';
-      str +='<div class="ad-location-img" style="background-image: url('+url+');">';
+      str +='<div class="area-location">';
+      str +='<div class="area-location-img" style="background-image: url('+url+');">';
       str +='<h3>'+data[i].Name+'</h3>';
       str +='<p>'+data[i].Zone+'</p>';
       str +='</div>';
-      str +='<div class="ad-location-info">';
+      str +='<div class="area-location-info">';
       str +='<ul>';
       str +='<li><img src="./assets/icons_clock.png" style="height:18px;margin:0px 4px;">'+data[i].Opentime+'</li>';
       str +='<li><img src="./assets/icons_pin.png" style="height:18px;margin:0px 6px;">'+data[i].Add+'</li>';
       str +='<li><img src="./assets/icons_phone.png" style="height:18px;margin:0px 8px;">'+data[i].Tel+'</li>';
       str +='</ul>';
       str +='</div>';
-      str +='<div class="ad-location-free">';
+      str +='<div class="area-location-free">';
       str +='<p><img src="./assets/icons_tag.png" style="height:18px;margin:0px 2px;">'+data[i].Ticketinfo+'</p>';
       str +='</div>';
       str +='</div>';
