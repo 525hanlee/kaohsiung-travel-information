8deae1648fbea9ff44dc4d709391240ca677d353
525hanlee <525hanlee@gmail.com>
2019-05-11 20:21:29 +0800

    Revert "Use Flex"
    This reverts commit 86834706a4e7573f5703a5a73580bcead59b22fb.

--- js/all.js
+++ js/all.js
@@ -1,13 +1,13 @@
-const data = mydata.result.records;
-const selectAd = document.querySelector('.select-ad');
-const btn = document.querySelectorAll('.hot-btn');
+var data = mydata.result.records;
+var selectAd = document.querySelector('.select-ad');
+var btn = document.querySelectorAll('.hot-btn');
 
 //加入行政區到列表
 function addToList(){
-  let str = '';
-  let zone = [];
+  var str = '';
+  var zone = [];
   str += '<option>--請選擇行政區--</option>';
-  for(let i = 0;i < data.length; i++){
+  for(var i = 0;i < data.length; i++){
     if(zone.indexOf(data[i].Zone) == -1){
       zone.push(data[i].Zone);
       str += '<option>'+data[i].Zone+'</option>';
@@ -18,11 +18,11 @@ function addToList(){
 
 //創建景點卡片
 function createCard(event){
-  const location = event.target.value;
-  let str = '';
+  var location = event.target.value;
+  var str = '';
   if (location == '--請選擇行政區--') {return;}
-  for (let i = 0; i < data.length; i++) {
-    let url = data[i].Picture1;
+  for (var i = 0; i < data.length; i++) {
+    var url = data[i].Picture1;
     url = url.replace("http", "https");
     if (data[i].Zone == location) {
       str +='<div class="card">';
@@ -54,6 +54,6 @@ addToList();
 //偵測到改變觸發函式
 selectAd.addEventListener('change',createCard);
 //掛監聽到按鈕
-for (let i = 0; i < btn.length; i++) {
+for (var i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click',createCard);
 }
